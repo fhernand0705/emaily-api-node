60902286e736fb50cf1a6f354904bedf5b28fcb2
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-07 16:05:49 -0400

    implemented route handler to handle user redirect from oauth provider (google)

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(
     new GoogleStrategy({
-        clientId: keys.googleClientID,
+        clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log({
+            'access token': accessToken,
+            'refresh token': refreshToken, 
+            'user-profile': profile
+        });
   })
 );
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; 
 
