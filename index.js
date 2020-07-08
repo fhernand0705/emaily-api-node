7b5ebd2cc675b4735ff792d628e0e39c33e8685a
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-08 12:48:41 -0400

    refactored contents of project directory to improve project structure

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require('express'); 
 const app = express();
-const passport = require('passport');
-const keys = require('./config/keys');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log({
-            'access token': accessToken,
-            'refresh token': refreshToken, 
-            'user-profile': profile
-        });
-  })
-);
+require('./services/passport'); 
+require('./routes/authRoutes')(app); 
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
 
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; 
 
