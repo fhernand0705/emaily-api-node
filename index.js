ebb8bc43d9526f7a6f0c9adfc88acc20078415c5
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-09 15:49:37 -0400

    installed cookie-session to persist data in app + cookie and passport configuration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express();
 const mongoose = require('mongoose'); 
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 require('./models/user');
@@ -8,6 +10,16 @@ require('./services/passport');
 require('./routes/authRoutes')(app); 
 
 mongoose.connect(keys.mongoURI);
+
+app.use(
+    cookieSession({
+        // required properties
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const PORT = process.env.PORT || 5000; 
