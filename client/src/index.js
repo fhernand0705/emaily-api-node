76ba2574c119ff507f24daf9acdd485de11e4d95
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-14 12:50:27 -0400

    installed redux, react-redux and react-router to manage state along with app routing + redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'; 
+import { createStore, applyMiddleware } from 'redux'; 
+
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware()); 
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
