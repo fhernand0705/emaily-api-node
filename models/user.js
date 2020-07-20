1310de2cc0f9f6c683b991a08f55bfd89ea68ec7
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-20 13:47:21 -0400

    modified user model to add credits property, and updated/saved credits per charge request + implemented middleware to check authentication of user before handling charge

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const { model } = mongoose;
 
 const userSchema = new Schema({
     googleId: String, 
-    name: String
+    name: String,
+    credits: { type: Number, default: 0 }
 });
 
 model('users', userSchema);
