870a97ac274c322338ccb4e624bcf864e671a837
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-21 16:25:46 -0400

    created new instance of survey model and defined properties based on body of request

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
-const requireCredits = require('../middleware/requireCredits')
+const requireCredits = require('../middleware/requireCredits');
+
+const Survey = mongoose.model('surveys');
+//const Recipient = mongoose.model('recipients');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        
+        const { title, subject, body, recipients } = req.body; 
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
     });
 }
