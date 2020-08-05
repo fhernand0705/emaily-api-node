226feb17658c4f6121248d386bf0d20a51e33c64
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-08-05 17:01:41 -0400

    sending updated list of surveys to client after survey deletion

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,16 +4,16 @@ module.exports = app => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }), (req,res) => console.log(res));
-    app.get(
-        '/auth/google/callback', 
-        passport.authenticate('google'), 
-        (req, res) => {
+
+    app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
         res.redirect('/surveys')
     });
+
     app.get('/api/logout', (req, res) => {
         req.logout();
         res.redirect('/');
-    })
+    });
+    
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
